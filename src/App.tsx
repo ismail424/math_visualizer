c65784d7904ae969026c3a5f8e474e7e6440f262
ismail424 <cicas03@hotmail.com>
2022-03-21 11:20:07 +0100

    Created start template and also the convertBase function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const convertBase = (value: number, from_base: number, to_base: number) => {
+  // The maximum number of digits we can convert is the length of the, example A = 10 and Z = 35
+  const DIGITS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  if (from_base < 2 || from_base > 36 || to_base < 2 || to_base > 36) {
+    // return NaN if base is not between 2 and 36
+    return null;
+  }
+
+  let result: string = '';
+  let number = value;
+
+  while (number > 0) {
+    result += DIGITS.charAt(number % to_base);
+    number = Math.floor(number / to_base);
+  }
+  return result;
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>{convertBase(255, 10, 16)}</p>
+    </>
   );
 }
 
